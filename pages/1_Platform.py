c75de159e177c1bfbf8bed9b37faee6d3e097a9f
Abdulrahman Tabaza <84684514+a-tabaza@users.noreply.github.com>
2023-07-26 05:56:58 +0300

    first commit

---
+++ pages/1_Platform.py
@@ -0,0 +1,197 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import csv
+import spss_converter
+import tempfile
+import os
+import utils
+
+st.title('Data and Modelling')
+st.subheader('Upload your data')
+uploaded_file = st.file_uploader('Choose a file', 
+                                type=['xls', 'xlsx', 'sav', 'csv', 'txt'])
+
+if uploaded_file is not None:
+
+    extention = uploaded_file.name.split('.')[1]
+
+    if extention in ['xls', 'xlsx']:
+        df = pd.read_excel(uploaded_file)
+
+    elif extention == 'sav':
+        st.write('If you are having issues, you can convert SPSS files here: https://secure.ncounter.de/spssconverter')
+        df, metadata = spss_converter.to_dataframe(uploaded_file.getvalue())
+
+    elif extention == 'csv':
+        st.write('If your file is not working correctly, specify the delimeter here:')
+        seperator = st.text_input('Seperator', ',')
+        df = pd.read_csv(uploaded_file, sep=seperator)
+
+    elif extention == 'txt':
+        st.write('If your file is not working correctly, specify the delimeter here:')
+        seperator = st.text_input('Seperator', ',')
+        df = pd.read_csv(uploaded_file, sep=seperator)
+
+    st.subheader('Data preview')
+
+    st.dataframe(df)
+    cols = df.columns.tolist()
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.write('Select predictive attributes')
+        pred = st.multiselect('Predictive attributes', cols)
+
+    cols = [x for x in cols if x not in pred]
+
+    with col2:
+        st.write('Select target/output attribute')
+        target = st.selectbox('Target attribute', cols)
+        
+    with col3:
+        st.write('Select the class of interest')
+        i_c = st.selectbox('Class of interest', df[target].unique().tolist())
+
+
+    new_df = df[pred + [target]]
+    class_of_interest = i_c
+    st.subheader('Data Preprocessing')
+    with st.expander('Missing Data Analysis Results', expanded=True):
+        missing = utils.has_nulls(df)
+        m_c = 'No missing values'
+        if(missing):
+            st.write('Total Missing Values:', utils.null_count(df))
+            m_c = st.radio('(MUST CHOOSE ONE) Missing Value Imputation Method:', 
+            ['None',
+             'Remove rows with missing values', 
+            'Let the assignment be made with the Random Forest method.'])
+        else:
+            st.write('No missing values found.')
+    
+    with st.expander('Outlier Value Analysis Result', expanded=True):
+        if utils.check_for_outliers(df) == 0:
+            outliers = 'No outliers'
+            st.write('No outlier values were detected in the data set.')
+        else:
+            st.write('Outlier values are detected in the data set. Total Outlier Values:', utils.check_for_outliers(df))
+            outliers = st.radio('Remove outliers?',['No', 'Yes'])
+    
+    with st.expander('Transformation Methods', expanded=True):
+        transformations = st.radio('Please choose one of the following methods for data transformation.',
+        ['None',
+         'Normalization', 
+        'Min-max Standardization', 
+        'Standardization', 
+        'Robust Standardization'])
+    
+    with st.expander('Attribute Selection Methods', expanded=True):
+        attribute_selection = st.radio('Please choose one of the following methods for attribute selection.',
+        ['None',
+         'Recursive Feature Elimination', 
+        'Based on Extra Trees Classifier',
+        'Based on Random Forest Classifier', 
+        'LASSO', 
+        'Boruta'])
+
+    with st.expander('Class Imbalance Analysis', expanded=True):
+        onehot_list = utils.categ_columns(df)
+        df = utils.encode_categorical_columns(df)
+        class_imbalance = 'None'
+        is_imb = utils.is_imbalance(df[target])
+        if is_imb and len(onehot_list) == 0:
+            class_imbalance = st.radio('There is a class imbalance problem in the dataset. Select one of the following methods to resolve the class imbalance problem.',
+            ['None','SMOTE','SMOTETomek'])
+        elif is_imb and len(onehot_list) != 0:
+            class_imbalance = st.radio('There is a class imbalance problem in the dataset. Select one of the following methods to resolve the class imbalance problem.',
+            ['None','SMOTE-NC'])
+        else:
+            st.write('There is no class imbalance problem in the dataset.')
+
+
+    with st.expander('Preprocessing Pipeline', expanded=True):
+        st.write('**Missing data:**', m_c)
+        if m_c == 'Remove rows with missing values.':
+            df = df.dropna()
+        if m_c == 'Let the assignment be made with the Random Forest method.':
+            df = utils.missing_forest_impute(df)
+        
+        st.write('**Remove outliers:**', outliers)
+        if outliers == 'Yes':
+            df = utils.drop_outliers(df)
+        
+        new_columns = df.columns[:-1]
+        st.write('**Attribute selection method:**', attribute_selection)
+        if attribute_selection == 'None':
+            new_columns = df.columns[:-1]
+
+        
+        st.write('**Data transformation:**', transformations)
+        if transformations == 'Normalization':
+            df = df.dropna()
+            transformed_x = utils.transform_features(df[list(new_columns)], 0)
+            df = pd.concat([transformed_x, df[target]], axis=1)
+        if transformations == 'Min-max Standardization':
+            df = df.dropna()
+            transformed_x = utils.transform_features(df[list(new_columns)], 1)
+            df = pd.concat([transformed_x, df[target]], axis=1)
+        if transformations == 'Standardization':
+            df = df.dropna()
+            transformed_x = utils.transform_features(df[list(new_columns)], 2)
+            df = pd.concat([transformed_x, df[target]], axis=1)
+        if transformations == 'Robust Standardization':
+            df = df.dropna()
+            transformed_x = utils.transform_features(df[list(new_columns)], 3)
+            df = pd.concat([transformed_x, df[target]], axis=1)
+        
+        target_col = df[target]
+        if attribute_selection == 'Recursive Feature Elimination':
+            df = df.dropna()
+            new_columns = utils.attr(df, df.drop([target], axis=1), df[target], 0)
+            df = df[new_columns]
+            df = pd.concat([df, target_col], axis=1)
+            st.write('**Important Attributes:**', ', '.join(new_columns))
+        if attribute_selection == 'Based on Extra Trees Classifier':
+            df = df.dropna()
+            new_columns = utils.attr(df, df.drop([target], axis=1), df[target], 1)
+            df = df[new_columns]
+            df = pd.concat([df, target_col], axis=1)
+            st.write('**Important Attributes:**', ', '.join(new_columns))
+        if attribute_selection == 'Based on Random Forest Classifier':
+            df = df.dropna()
+            new_columns = utils.attr(df, df.drop([target], axis=1), df[target], 2)
+            df = df[new_columns]
+            df = pd.concat([df, target_col], axis=1)
+            st.write('**Important Attributes:**', ', '.join(new_columns))
+        if attribute_selection == 'LASSO':
+            df = df.dropna()
+            new_columns = utils.attr(df, df.drop([target], axis=1), df[target], 3)
+            df = df[new_columns]
+            df = pd.concat([df, target_col], axis=1)
+            st.write('**Important Attributes:**', ', '.join(new_columns))
+        if attribute_selection == 'Boruta':
+            df = df.dropna()
+            new_columns = utils.attr(df, df.drop([target], axis=1), df[target], 4)
+            df = df[new_columns]
+            df = pd.concat([df, target_col], axis=1)
+            st.write('**Important Attributes:**', ', '.join(new_columns))
+
+        columns_ = df.columns
+        X, y = df.drop([target], axis=1), df[target]
+        df = pd.concat([X, y], axis=1)
+
+        if class_imbalance != 'None':
+            st.write('**Class imbalance handling strategy:**', class_imbalance)
+            df = df.dropna()
+            if class_imbalance == 'SMOTE':
+                X, y = utils.smote_function(df, df.drop([target], axis=1), df[target], 0)
+            if class_imbalance == 'SMOTETomek':
+                X, y = utils.smote_function(df, df.drop([target], axis=1), df[target], 1)
+            if class_imbalance == 'SMOTE-NC':
+                X, y = utils.smote_function(df, df.drop([target], axis=1), df[target], 2)
+
+        columns_ = df.columns
+        X, y = df.drop([target], axis=1), df[target]
+        df = pd.concat([X, y], axis=1)
+
+        st.dataframe(df)
---
+++ utils.py
@@ -0,0 +1,363 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neighbors import LocalOutlierFactor
+from collections import Counter
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split, KFold, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import RobustScaler
+from imblearn.over_sampling import SMOTENC
+import io
+import pandas as pd
+from sklearn.model_selection import RepeatedStratifiedKFold
+import fontawesome as fa
+from lightgbm import LGBMClassifier
+import pyreadstat
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import base64
+import io
+from imblearn.over_sampling import SMOTE
+import json
+import plotly.figure_factory as ff
+from sklearn.model_selection import train_test_split, KFold, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.impute import SimpleImputer
+import plotly.graph_objs as go
+import os
+from plotly import tools
+from collections import Counter
+import datetime
+import fontawesome as fa
+import math
+from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+import sys
+import sklearn.neighbors._base
+sys.modules['sklearn.neighbors.base'] = sklearn.neighbors._base
+from missingpy import MissForest
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import RFE
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
+from boruta import BorutaPy
+from sklearn.linear_model import Lasso, LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn import metrics
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import matthews_corrcoef
+##randomforest
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.tree import DecisionTreeClassifier
+import lightgbm as lgb
+from sklearn import tree
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import AdaBoostClassifier
+import xgboost as xgb
+import sklearn_json as skljson
+import shap
+import sklearn
+import matplotlib
+import matplotlib.pyplot as pl
+import matplotlib.pylab as plt
+from imblearn.combine import SMOTETomek
+from imblearn.under_sampling import TomekLinks
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn import datasets, ensemble, model_selection
+from sklearn.datasets import make_multilabel_classification
+from sklearn.metrics import plot_confusion_matrix
+from io import BytesIO
+import sys
+from sklearn.model_selection import RepeatedKFold
+
+def has_nulls(df):
+    nulls = df.isna().sum().sum()
+    if nulls > 0:
+        return True
+    return False
+    
+def null_count(df):
+    return df.isna().sum().sum()
+
+def has_categ_columns(df):
+    for col in df.columns:
+        if df[col].dtype.name in ["category", "object"]:
+            return True
+    return False
+
+def categ_columns(df):
+    if(not has_categ_columns):
+        return []
+    categ_columns = []
+    for col in df.columns:
+        if df[col].dtype.name in ["category", "object"]:
+            categ_columns.append(col)
+    return categ_columns
+
+
+def encode_categorical_columns(df):
+    for col in df.columns:
+        if df[col].dtype.name in ["category", "object"]:
+            df[col] = df[col].astype("category")
+            df[col] = df[col].cat.codes
+    return df
+
+def numerical_columns(df):
+    numerical_columns = []
+    for col in df.columns:
+        if pd.api.types.is_numeric_dtype(df[col]):
+            numerical_columns.append(col)
+    return numerical_columns   
+
+
+
+
+def check_for_outliers(df):
+    df = encode_categorical_columns(df)
+    df = df.dropna()
+    
+    numeric_columns = numerical_columns(df)
+    if not numeric_columns:
+        return []
+
+    lof = LocalOutlierFactor()
+    lof.fit(df[numeric_columns])
+
+    yhat = lof.fit_predict(df[numeric_columns])
+    outliers = np.where(yhat == -1)[0]
+
+    return len(outliers)
+
+
+def is_imbalance(y):
+    # count the frequency of each class
+    count = Counter(y)
+    aa=list(count.values())
+    deger = False
+    for i in range(len(aa)-1):
+        if aa[i] >= (3*aa[i+1]) or 3*aa[i] <= aa[i+1]:
+            deger=True
+    return deger
+
+
+def impute_missing(df):
+    # missing data
+    cols = df.columns
+    def cleanveri(veri):
+        try:
+            veri = float(veri)
+        except:
+            veri = veri
+        if isinstance(veri, str):
+            return np.nan
+        elif isinstance(veri, float):
+            return veri
+    for i in df.columns:
+        df[i] = df[i].apply(lambda x : cleanveri(x))
+
+    #df = df.to_numpy()
+    imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean' )
+    imp_mean.fit(df)
+    df = pd.DataFrame(imp_mean.transform(df))
+    df.columns = cols
+    return df
+
+def drop_outliers(x):
+    
+    dff = encode_categorical_columns(x)
+    dff = impute_missing(dff)
+
+    lof = LocalOutlierFactor()
+    yhat = lof.fit_predict(dff)
+    mask = yhat != -1 ##yine burada aykırı değer içeren satırları bulduk
+    aykirideger= 0
+    aykiridegerlist=[]
+    for i in range(len(mask)):
+        if mask[i] != True:
+            aykirideger = aykirideger+1
+            aykiridegerlist.append(i)
+    x = x.drop(aykiridegerlist, axis=0)
+    x = x.reset_index(drop=True)
+    return x  
+
+def attr(df, X, y, a):
+    if a==0:
+        model = LogisticRegression(solver='lbfgs')
+        rfe = RFE(model)
+        fit = rfe.fit(X, y)
+        df=rfe.transform(X)
+        new_columns = list(X.columns[rfe.support_])
+
+    elif a==1:
+        clf = ExtraTreesClassifier(n_estimators=50)
+        fit = clf.fit(X, y)
+        clf.feature_importances_
+        model = SelectFromModel(clf, prefit=True)
+        feature_idx = model.get_support()
+        new_columns = list(X.columns[feature_idx])
+
+    elif a==2:
+        sel = SelectFromModel(RandomForestClassifier(n_estimators = 100))
+        sel.fit(X, y)
+        sel.get_support()
+        feature_idx = sel.get_support()
+        new_columns = list(X.columns[feature_idx])
+        df = sel.transform(X)
+
+    elif a==3:
+        sel = SelectFromModel(LogisticRegression(C=1, penalty='l1', solver='liblinear'))
+        sel.fit(X, np.ravel(y,order='C'))
+        feature_idx = sel.get_support()
+        X=pd.DataFrame(X,  columns=X.columns)
+        new_columns = list(X.columns[feature_idx])
+        df = sel.transform(X)
+
+    elif a==4:
+        rfc = RandomForestClassifier(random_state=1, n_estimators=1000, max_depth=5)
+        boruta_selector = BorutaPy(rfc, n_estimators='auto', verbose=2, random_state=1)
+        boruta_selector.fit(np.array(X), np.array(y))
+        a=X.columns[boruta_selector.support_]
+        new_columns=list(a)
+        df = boruta_selector.transform(np.array(X))
+
+    return new_columns
+
+def transform_features(x, a):
+    # data transformation function
+
+    try:
+        dff = encode_categorical_columns(x)
+    except:
+        dff =x
+    # dff = kayipveriler(dff)
+    if a==0:
+        normalizer = preprocessing.Normalizer()
+        list = []
+        for i in dff.columns:
+            threshold = 10
+            if dff[i].nunique() < threshold:
+                list.append(i)
+        adf = dff.copy()
+        normalizer = preprocessing.Normalizer().fit(adf)
+        adf= normalizer.transform(adf)
+        xcolumns = dff.columns.values
+        adf = pd.DataFrame(adf)
+        for i in range(len(xcolumns)):
+            adf= adf.rename(columns={i:xcolumns[i]})
+        for i in list:
+            adf[i] = dff[i]
+        dff = adf
+
+    if a==1:
+        std = MinMaxScaler()
+        list = []
+        for i in dff.columns:
+            threshold = 10
+            if dff[i].nunique() < threshold:
+                list.append(i)
+
+
+
+        adf = dff.copy()
+        std = std.fit(adf)
+        adf= std.transform(adf)
+        xcolumns = dff.columns.values
+        adf = pd.DataFrame(adf)
+        for i in range(len(xcolumns)):
+            adf= adf.rename(columns={i:xcolumns[i]})
+        for i in list:
+            adf[i] = dff[i]
+        dff = adf
+
+    if a==2:
+        std = StandardScaler()
+        list = []
+        for i in dff.columns:
+            threshold = 10
+            if dff[i].nunique() < threshold:
+                list.append(i)
+
+        adf = dff.copy()
+        std = std.fit(adf)
+        adf= std.transform(adf)
+        xcolumns = dff.columns.values
+        adf = pd.DataFrame(adf)
+        for i in range(len(xcolumns)):
+            adf= adf.rename(columns={i:xcolumns[i]})
+        for i in list:
+            adf[i] = dff[i]
+        dff = adf
+
+    if a==3:
+        std = RobustScaler()
+        list = []
+        for i in dff.columns:
+            threshold = 10
+            if dff[i].nunique() < threshold:
+                list.append(i)
+
+        adf = dff.copy()
+        std = std.fit(adf)
+        adf= std.transform(adf)
+        xcolumns = dff.columns.values
+        adf = pd.DataFrame(adf)
+        for i in range(len(xcolumns)):
+            adf= adf.rename(columns={i:xcolumns[i]})
+        for i in list:
+            adf[i] = dff[i]
+        dff = adf
+
+
+    return dff
+
+def missing_forest_impute(x):
+    imputer = MissForest()
+    x = imputer.fit_transform(x,cat_vars=None)
+    return x
+
+
+
+def smote_function(df,X,y, a):
+
+    X =  X.to_numpy()
+
+    categorical_features = np.argwhere(np.array([len(set(X[:,x])) for x in range(X.shape[1])]) <= 9).flatten()
+    ##normalde buradaki <=10 du, bu haliyle bazı sayısalları kategorik yapıyor veya cinsiyeti kategorik görmüyor vs, bağlanıp bakmamız gerek aslında
+
+    if a==0:
+        sm = SMOTE()
+        X, y = sm.fit_resample(X, y)
+    elif a==1:
+        sm = SMOTETomek(sampling_strategy='not majority')
+        X, y = sm.fit_resample(X, y)
+
+    elif a==3 and len(categorical_features) !=0:
+        sm = SMOTENC(categorical_features=categorical_features, sampling_strategy='not majority')
+        X, y = sm.fit_resample(X, y)
+    elif a==3 and len(categorical_features) ==0:
+        sm = SMOTE()
+        X, y = sm.fit_resample(X, y)
+
+
+    return X,y
